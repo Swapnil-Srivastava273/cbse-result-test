6613adfc5fb1560c72a72b7d6ff85b87436a04a7
Swapnil-Srivastava273 <31892718+Swapnil-Srivastava273@users.noreply.github.com>
2019-05-05 13:01:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ let check=()=>{
             }else{
                 console.log(resp.headers);
                 console.log(data);
-                check();
             }    
 
             console.log(`Tried for ${++count} th time at ${(new Date()).toString()}`);
