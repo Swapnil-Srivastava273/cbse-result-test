f1e4ff189cf13e6acfd90c64a0e22c2b3c305f2b
Swapnil-Srivastava273 <31892718+Swapnil-Srivastava273@users.noreply.github.com>
2019-05-05 14:09:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const cbse="http://cbseresults.nic.in/cbseresults_cms/Public/Home.aspx";
 const http=require("http");
+const vm=require("vm");
 let count=0;
 let changed=false;
 let changedDate=null;
 let interval=null;
-let check=()=>{
-    http.get({'host':'cbseresults.nic.in','path':'/cbseresults_cms/Public/Home.aspx','headers':{'user-agent':'/cbseresults_cms/Public/Home.aspx','pragma':'no-cache','cache-control':'no-cache','referer':'https://www.google.com/',"cookie":"ASP.NET_SessionId=vegxnc0xxcu54z4rvotbabay"}},resp=>{
+let check=(path='/cbseresults_cms/Public/Home.aspx')=>{
+    http.get({'host':'cbseresults.nic.in','path':path,'headers':{'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36','pragma':'no-cache','cache-control':'no-cache','referer':'https://www.google.com/',"cookie":"ASP.NET_SessionId=vegxnc0xxcu54z4rvotbabay"}},resp=>{
         let data="";
         resp.on('data',chunk=>{
             data+=chunk;
@@ -23,7 +24,20 @@ let check=()=>{
                     console.log("Some stupid redirect ?");
                     console.log(data);
                     console.log(resp.headers);
-                }else{
+                    let sandbox={
+                        y:{
+                            location:{
+                                assign:function(x){this.loc=x;}
+                            },
+                            loc:""
+                        }
+                    };
+                    vm.createContext(sandbox);
+                    let code=data.match(/<\/script><script>[^<]*<\/script><\/html>/)[0].slice(17,-16);
+                    vm.runInContext("y.location.assign=function(x){y.loc=x;};"+code,sandbox);
+                    //console.log(sandbox.y.loc);
+                    check(sandbox.y.loc);
+                    }else{
                     console.log("still no change :c");
                 }
             }else{
@@ -32,7 +46,7 @@ let check=()=>{
                 console.log(resp.statusCode);
                 //check();
             }    
-
+            console.log(path);
             console.log(`Tried for ${++count} th time at ${(new Date()).toString()}`);
         });
     });
@@ -45,4 +59,3 @@ http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/plain"});
     res.end(`${count} ${changed} ${changed?changedDate:''}`);
 }).listen(process.env.PORT||5000);
-
